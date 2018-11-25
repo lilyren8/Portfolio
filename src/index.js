846647c307b09febaf864f5ac92340abafd90724
Bo Zhao <bodotzhao@gmail.com>
2018-11-25 15:23:45 -0500

    Made header sticky

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ import Notes from "./screens/Notes"
 import JHI from "./screens/JHI"
 import Inforum from "./screens/Inforum"
 import ChatWindow from "./components/chatWindow"
-import { Layout, Header, Navigation, Drawer } from "react-mdl"
+import { Layout, Header, Navigation, Content } from "react-mdl"
+import { StickyContainer, Sticky } from "react-sticky"
 
 const styles = {
   link: {
@@ -25,73 +26,80 @@ const styles = {
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div style={{ height: "1200px", position: "relative" }}>
-          <Layout
-            style={{
-              background: "#FAFAFA",
-              fontFamily: "futura-lt-w01-book",
-              fontSize: 15,
-              userSelect: "text"
-            }}
-          >
-            <Header
-              transparent
-              title={
-                <div style={{ width: 200 }}>
-                  <Link to="/" style={{ color: "black", textDecoration: "none" }}>
-                    Lily Ren
-                  </Link>
-                  <a style={{ paddingLeft: 10 }} href="https://www.linkedin.com/in/lily-ren/">
-                    <i className="fa fa-linkedin-square" style={{ color: "white", fontSize: 25 }} />
-                  </a>
-                  <a style={{ paddingLeft: 10 }} href="https://github.com/lilyren8">
-                    <i className="fa fa-github" style={{ color: "white", fontSize: 25 }} />
-                  </a>
-                </div>
-              }
-              style={{ backgroundColor: "#ffffff", color: "yellow" }}
-            >
-              <Navigation>
-                <Link style={styles.link} to="/">
-                  Home
-                </Link>
-                <Link style={styles.link} to="/resume/">
-                  Resume
-                </Link>
-                <Link style={styles.link} to="/contact/">
-                  Contact
-                </Link>
-              </Navigation>
-            </Header>
+      <StickyContainer>
+        <Router>
+          <div>
+            <div style={{ height: "70px", position: "relative" }}>
+              <Layout
+                style={{
+                  fontFamily: "futura-lt-w01-book",
+                  fontSize: 15,
+                  userSelect: "text",
+                  zIndex: 3
+                }}
+              >
+                <Sticky>
+                  {({ style }) => (
+                    <header style={style}>
+                      <Header
+                        title={
+                          <div style={{ width: 200 }}>
+                            <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+                              Lily Ren
+                            </Link>
+                            <a
+                              style={{ paddingLeft: 10 }}
+                              href="https://www.linkedin.com/in/lily-ren/"
+                            >
+                              <i
+                                className="fa fa-linkedin-square"
+                                style={{ color: "white", fontSize: 25 }}
+                              />
+                            </a>
+                            <a style={{ paddingLeft: 10 }} href="https://github.com/lilyren8">
+                              <i
+                                className="fa fa-github"
+                                style={{ color: "white", fontSize: 25 }}
+                              />
+                            </a>
+                          </div>
+                        }
+                      >
+                        <Navigation>
+                          <Link style={styles.link} to="/">
+                            Home
+                          </Link>
+                          <Link style={styles.link} to="/resume/">
+                            Resume
+                          </Link>
+                          <Link style={styles.link} to="/contact/">
+                            Contact
+                          </Link>
+                        </Navigation>
+                      </Header>
+                    </header>
+                  )}
+                </Sticky>
 
-            {/* side menu */}
-            <Drawer title="Title">
-              <Navigation>
-                <Link style={styles.link} to="/resume/">
-                  Resume
-                </Link>
-                <Link style={styles.link} to="/contact/">
-                  Contact
-                </Link>
-              </Navigation>
-            </Drawer>
+                <ChatWindow />
+              </Layout>
+            </div>
 
             {/* main content */}
-            <Route path="/" exact component={Home} />
-            <Route path="/resume/" component={Resume} />
-            <Route path="/contact/" component={Contact} />
-            <Route path="/chatbot/" component={Chatbot} />
-            <Route path="/iclinic/" component={iClinic} />
-            <Route path="/greenp/" component={GreenP} />
-            <Route path="/notes/" component={Notes} />
-            <Route path="/jhi/" component={JHI} />
-            <Route path="/inforum/" component={Inforum} />
-
-            <ChatWindow />
-          </Layout>
-        </div>
-      </Router>
+            <Content>
+              <Route path="/" exact component={Home} />
+              <Route path="/resume/" component={Resume} />
+              <Route path="/contact/" component={Contact} />
+              <Route path="/chatbot/" component={Chatbot} />
+              <Route path="/iclinic/" component={iClinic} />
+              <Route path="/greenp/" component={GreenP} />
+              <Route path="/notes/" component={Notes} />
+              <Route path="/jhi/" component={JHI} />
+              <Route path="/inforum/" component={Inforum} />
+            </Content>
+          </div>
+        </Router>
+      </StickyContainer>
     )
   }
 }
