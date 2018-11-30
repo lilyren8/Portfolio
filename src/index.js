5ca7d139dd1bb18f51f19fbb5c0332b152475c06
Bo Zhao <bodotzhao@gmail.com>
2018-11-29 23:10:56 -0500

    Add mobile header

--- src/index.js
+++ src/index.js
@@ -28,92 +28,126 @@ const styles = {
 }
 
 class App extends React.Component {
+  defaultContent = () => {
+    return (
+      <StickyContainer>
+        <Router>
+          <div>
+            <div style={{ height: "70px", position: "relative" }}>
+              <Layout
+                style={{
+                  fontFamily: "futura-lt-w01-book",
+                  fontSize: 15,
+                  userSelect: "text",
+                  zIndex: 3
+                }}
+              >
+                <Sticky>
+                  {({ style }) => (
+                    <header style={style}>
+                      <Header
+                        title={
+                          <div style={{ width: 200 }}>
+                            <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+                              Lily Ren
+                            </Link>
+                            <a
+                              style={{ paddingLeft: 10 }}
+                              href="https://www.linkedin.com/in/lily-ren/"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <i
+                                className="fa fa-linkedin-square"
+                                style={{ color: "white", fontSize: 25 }}
+                              />
+                            </a>
+                            <a
+                              style={{ paddingLeft: 10 }}
+                              href="https://github.com/lilyren8"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <i
+                                className="fa fa-github"
+                                style={{ color: "white", fontSize: 25 }}
+                              />
+                            </a>
+                          </div>
+                        }
+                      >
+                        <Navigation>
+                          <Link style={styles.link} to="/">
+                            Home
+                          </Link>
+                          <Link style={styles.link} to="/resume/">
+                            Resume
+                          </Link>
+                          <Link style={styles.link} to="/contact/">
+                            Contact
+                          </Link>
+                        </Navigation>
+                      </Header>
+                    </header>
+                  )}
+                </Sticky>
+              </Layout>
+            </div>
+
+            {/* main content */}
+            <Content>
+              <ChatWindow />
+              <Route path="/" exact component={Home} />
+              <Route path="/resume/" component={Resume} />
+              <Route path="/contact/" component={Contact} />
+              <Route path="/chatbot/" component={Chatbot} />
+              <Route path="/iclinic/" component={iClinic} />
+              <Route path="/greenp/" component={GreenP} />
+              <Route path="/notes/" component={Notes} />
+              <Route path="/jhi/" component={JHI} />
+              <Route path="/inforum/" component={Inforum} />
+            </Content>
+          </div>
+        </Router>
+      </StickyContainer>
+    )
+  }
+
+  mobileContent = () => {
+    return (
+      <div>
+        <StickyContainer>
+          <Sticky>
+            {({ style }) => (
+              <header style={style}>
+                <div
+                  style={{
+                    height: "40px",
+                    backgroundColor: "#4153AF",
+                    paddingTop: 10,
+                    paddingLeft: 10
+                  }}
+                >
+                  <i className="fa fa-bars" style={{ color: "white", fontSize: 25 }} />
+                  <span style={{ paddingRight: 10 }}>Lily Ren</span>
+                  <a href="https://www.linkedin.com/in/lily-ren/">
+                    <i className="fa fa-linkedin-square" style={{ color: "white", fontSize: 25 }} />
+                  </a>
+                </div>
+              </header>
+            )}
+          </Sticky>
+          <div style={{ height: 1200 }} />
+        </StickyContainer>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        <Mobile>Mobile</Mobile>
-        <Default>
-          <StickyContainer>
-            <Router>
-              <div>
-                <div style={{ height: "70px", position: "relative" }}>
-                  <Layout
-                    style={{
-                      fontFamily: "futura-lt-w01-book",
-                      fontSize: 15,
-                      userSelect: "text",
-                      zIndex: 3
-                    }}
-                  >
-                    <Sticky>
-                      {({ style }) => (
-                        <header style={style}>
-                          <Header
-                            title={
-                              <div style={{ width: 200 }}>
-                                <Link to="/" style={{ color: "black", textDecoration: "none" }}>
-                                  Lily Ren
-                                </Link>
-                                <a
-                                  style={{ paddingLeft: 10 }}
-                                  href="https://www.linkedin.com/in/lily-ren/"
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  <i
-                                    className="fa fa-linkedin-square"
-                                    style={{ color: "white", fontSize: 25 }}
-                                  />
-                                </a>
-                                <a
-                                  style={{ paddingLeft: 10 }}
-                                  href="https://github.com/lilyren8"
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  <i
-                                    className="fa fa-github"
-                                    style={{ color: "white", fontSize: 25 }}
-                                  />
-                                </a>
-                              </div>
-                            }
-                          >
-                            <Navigation>
-                              <Link style={styles.link} to="/">
-                                Home
-                              </Link>
-                              <Link style={styles.link} to="/resume/">
-                                Resume
-                              </Link>
-                              <Link style={styles.link} to="/contact/">
-                                Contact
-                              </Link>
-                            </Navigation>
-                          </Header>
-                        </header>
-                      )}
-                    </Sticky>
-                  </Layout>
-                </div>
-
-                {/* main content */}
-                <Content>
-                  <ChatWindow />
-                  <Route path="/" exact component={Home} />
-                  <Route path="/resume/" component={Resume} />
-                  <Route path="/contact/" component={Contact} />
-                  <Route path="/chatbot/" component={Chatbot} />
-                  <Route path="/iclinic/" component={iClinic} />
-                  <Route path="/greenp/" component={GreenP} />
-                  <Route path="/notes/" component={Notes} />
-                  <Route path="/jhi/" component={JHI} />
-                  <Route path="/inforum/" component={Inforum} />
-                </Content>
-              </div>
-            </Router>
-          </StickyContainer>
-        </Default>
+        <Mobile>{this.mobileContent()}</Mobile>
+        <Default>{this.defaultContent()}</Default>
       </div>
     )
   }
